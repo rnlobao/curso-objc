876560f6ca0789306194c219d4736221f4c1b13f
Robson Novato <rnlobao42@gmail.com>
2023-09-09 16:21:43 -0300

    primeiro projeto

---
+++ MyApp/MyApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by Robson Novato on 09/09/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+- (IBAction)pressMe:(id)sender;
+
+
+@end
+
---
+++ MyApp/MyApp/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  MyApp
+//
+//  Created by Robson Novato on 09/09/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)pressMe:(id)sender {
+    
+}
+@end
